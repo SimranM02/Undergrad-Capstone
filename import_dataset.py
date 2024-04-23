9f14d9d7d4e717dd0e146977ace0e58c962c4891
SimM <164609524+SimranM02@users.noreply.github.com>
2024-04-23 17:43:33 +0530

    Create import_dataset.py

---
+++ import_dataset.py
@@ -0,0 +1,8 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+#####
+!gdown https://drive.google.com/uc?id=1sjBXm2WzKkfbPWIbZD0YfEsq78CfgGwu
+
+####
+!unzip "/content/drive/MyDrive/Colab Notebooks/xray-dataset.zip" -d "/content/"
